b1ac6c33b484a8816a879ff3c7e008800087ff6f
Rostyslav Sova <sova.rostislav@gmail.com>
2018-02-28 13:41:41 +0000

    Last version

--- js/index.js
+++ js/index.js
@@ -1,32 +1,34 @@
 const a = parseFloat(prompt('Enter a'));
 const b = parseFloat(prompt('Enter b'));
 const c = parseFloat(prompt('Enter c'));
-solveQuadraticEquation(a,b,c)();
-
-
+let answer =[];
+solveQuadraticEquation(a,b,c);
+document.write('For your coefficient: ' + a + ', ' + b + ', ' + c + ';<br>'
+          +'Aswer is: '+answer[0] +' and '+answer[1] );
 
 function solveQuadraticEquation (a,b,c) {
 // if one of coefficient = 0  
+    if (a === 0 && b === 0 && c === 0){
+        answer[0]=answer[1]=0;
+        return
+    }
     if (a === 0 || b === 0 || c === 0) {
         if (a === 0){
             const x = (c * -1) / b;
-            document.write('For your coefficient: '+a+', '+b+', '+c+'<br>'
-            +'Aswer is: '+'X= ' + x);
+            answer[0] = answer[1] =x;
             return;
         } else if (b === 0) {
             if (-c/a < 0){
-                document.write('For your coefficient: ' + a + ', ' + b + ', ' + c + '<br>'
-                +'Aswer is: '+'that equation has no real roots');
+              answer[0]='that equation has no real roots';
+              answer[1]='';
                 return;
             }
             const x = Math.sqrt((c*-1) / a);
-            document.write('For your coefficient: ' + a + ', ' + b + ', ' + c + '<br>'
-                +'Aswer is: '+'X1 and X2 ' + x);
+           answer[0] = answer[1] =x;
                 return;
         } else if (c === 0) {
             const x =(b*-1) / a;
-            document.write('For your coefficient: ' + a + ', ' + b + ', ' + c + '<br>'
-              +'Aswer is: '+'X= ' + x);
+         answer[0] = answer[1] =x;
             return;
         }
     }else {
@@ -34,18 +36,17 @@ function solveQuadraticEquation (a,b,c) {
         const d = discriminant(a,b,c);
         if (d === 0) {
             const x = (b * -1) / (2 * a);
-            document.write('For your coefficient: ' + a + ', ' + b + ', ' + c + '<br>'
-                +'Aswer is: '+'X1 and X2= ' + x);
+            answer[0] = answer[1] = x;
             return;
         } else if (d > 0) {
             const x1 = ((b * -1) + Math.sqrt(d)) / (2 *a);
             const x2 = ((b * -1) - Math.sqrt(d)) / (2 *a);
-            document.write('For your coefficient: ' + a + ', ' + b + ', ' + c + '<br>'
-              +'Aswer is: '+'X1= ' + x1 +'; X2= '+ x2);
+            answer[0]=x1;
+              answer[1]=x2;
              return;
         } 
-        document.write('For your coefficient: ' + a + ', ' + b + ', ' + c + '<br>'
-          +'Aswer is: '+'Discriminant < 0, You need to use a formula with complex numbers.' + '<a href="https://en.wikipedia.org/wiki/Quadratic_equation">Quadratic equation</a>');
+            answer[0]='Discriminant < 0, You need to use a formula with complex numbers.';
+            answer[1]='<a href="https://en.wikipedia.org/wiki/Quadratic_equation">Quadratic equation</a>';
         return;
         
     }
